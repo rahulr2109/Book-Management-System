0ec7028cd35dc22031d78b9a672ffab9d941c7ce
Rahul Rai <rahul0921r@gmail.com>
2024-04-26 01:48:17 +0530

    error resolved

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://rahul:rahul@atlascluster.y0sdodk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+// export const mongoDBURL =
+//   "mongodb+srv://rahul:rahul@atlascluster.y0sdodk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+export const mongoDBURL = "mongodb://mongodb:27017/db";
